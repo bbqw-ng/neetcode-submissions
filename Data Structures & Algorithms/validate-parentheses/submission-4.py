c032baada82f8ba541813590e7667b186f0ecb2f
Brian Wang <brwng902@gmail.com>
2026-07-21 20:24:48 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.py
@@ -0,0 +1,35 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open = ['[', '{', '(']
+        if len(s) == 1:
+            return False
+        #declare a stack
+        stack = []
+        # we go through the list, adding any open brackets we see,
+        for i in s:
+            print(i)
+            if i in open:
+                stack.append(i)
+        #if hit close bracket then we want to counter check the list to see if its a valid open (so we would check the last element in the stack)
+            else:
+                if not stack:
+                    return False
+                top = stack[-1]
+                print(top)
+                if i == ')' and top == '(':
+                    stack.pop()
+                elif i == ']' and top == '[':
+                    print(i)
+                    stack.pop()
+                elif i == '}' and top == '{':
+                    stack.pop()
+                else:
+                    return False
+        if not stack:
+            return True
+        else:
+            return False
+        #true: pop from stack, continue
+        #false: return false, termiante immediately.
+
+        
